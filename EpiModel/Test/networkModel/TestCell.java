a5cfb7278f7847828db489aa574f56e9a47dc8ea
ducng <ducng@10.32.79.134>
2017-03-24 20:24:33 -0700

    Finished testCell unit test, untested

--- EpiModel/Test/networkModel/TestCell.java
+++ EpiModel/Test/networkModel/TestCell.java
@@ -6,61 +6,78 @@ import diseases.Disease;
 public class TestCell {
 	
 
-	public static void testInfectedUpdate(Cell mock)
+	public static void infectedUpdateTest(Cell mock)
 	{
-		testInfect(mock, new Disease(.1,.1,.1,Conditions.SUSCEPTIBLE));
+		infectTest(mock, new Disease(.1,.1,.1,Conditions.SUSCEPTIBLE));
 		Cell oldState = new Cell(mock);
 		Cell newCell = mock.infectedUpdate();
 		assert mock.equals(oldState) : "Mock has been modified";
 		assert newCell.condition == mock.disease.getRetCondition() || 
-				newCell.condition == Conditions.INFECTED: "Condition's state is unexpected";
+				newCell.condition == Conditions.INFECTED
+				: "Condition's state is unexpected";
 		assert newCell.infectTime == 0 || newCell.infectTime == mock.infectTime+1 : "infect time not updated";
+		
+		System.out.println("infectedUpdate test passed");
 	}
 	
 	public static void testImmuneUpdate()
 	{
 		
+	}
+	
+	public static void susceptibleUpdateTest(Cell mock, World mockWorld)
+	{
+		int location = findCellLocation(mock, mockWorld);
+		Cell oldState = new Cell(mock);
+		Cell newCell = mock.susceptibleUpdate(mockWorld.world, mockWorld.connections.get(location));
+		assert mock.equals(oldState) : "Mock has been modified";
+		assert newCell.condition == Conditions.SUSCEPTIBLE
+				|| newCell.condition == Conditions.INFECTED
+				|| newCell.condition== Conditions.EXPOSED
+				: "Final Condition impossible";
 		
+		System.out.println("susceptibleUpdate test passed.");
 		
 	}
 	
-	public static void testSusceptibleUpdate()
+	public static void removedUpdateTest(Cell mock, World mockWorld)
 	{
-		
-	}
-	
-	public static void testRemovedUpdate(Cell mock, World mockWorld)
-	{
-		int location = 0;
-		for(int i = 0; i < mockWorld.world.size(); i++)
-		{
-			if(mockWorld.world.get(i)==mock) break;
-			if(i==mockWorld.world.size()-1) assert false : "World does not contained specified cell"; 
-			location++;
-		}
+		int location = findCellLocation(mock, mockWorld);
 		Cell oldState = new Cell(mock);
 		Cell newCell = mock.immuneUpdate(mockWorld.world, mockWorld.connections.get(location));
 		assert mock.equals(oldState);
 		assert newCell.condition == Conditions.REMOVED || 
-				newCell.condition == Conditions.SUSCEPTIBLE : "Final condition not possible";
-	}
-	
-	public static void testDoesWhatOnUpdate()
-	{
+				newCell.condition == Conditions.SUSCEPTIBLE
+				: "Final condition not possible";
 		
+		System.out.println("removedUpdate test passed");
 	}
 	
-	public static void testRecover()
+	public static void recoverTest()
 	{
 		
 	}
 	
 	//not a pure function, edits stuff
-	public static void testInfect(Cell mock, Disease disease)
+	public static void infectTest(Cell mock, Disease disease)
 	{
 		mock.infect(disease);
 		assert mock.condition == Conditions.INFECTED : "Could not infect Cell";
 		assert mock.infectTime == 0 : "infectTime was not reset to zero";
 		assert mock.disease == disease : "Disease was not added to mock cell obj";
+		
+		System.out.println("infect test passed");
+	}
+	
+	public static int findCellLocation(Cell cell, World world)
+	{
+		int location = 0;
+		for(int i = 0; i < world.world.size(); i++)
+		{
+			if(world.world.get(i)==cell) break;
+			if(i==world.world.size()-1) assert false : "World does not contained specified cell"; 
+			location++;
+		}
+		return location;
 	}
 }
--- EpiModel/Test/networkModel/TestWorld.java
+++ EpiModel/Test/networkModel/TestWorld.java
@@ -2,4 +2,5 @@ package networkModel;
 
 public class TestWorld {
 
+	
 }
